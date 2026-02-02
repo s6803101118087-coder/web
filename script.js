56d64de00cabc80d3168f741c21a4c698d38eec3
s6803101118087-coder <s6803101118087@email.kmutnb.ac.th>
2026-02-02 11:27:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById("p1").textContent = "ผมว่าคนนี้เป็น Imposter";
 document.getElementById("p2").textContent = "BTW"
-document.getElementById("p3").textContent = "คุณมึงเหม็นมาก"หเหม็นมากหม็นมาก็นมากเหม็นมาก"
-
+document.getElementById("p3").textContent = "คุณมึงเหม็นมาก"
